213b14c3bfaad13bbdc336a7007f5ee114dccadb
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 18:53:11 +0200

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * mul - multiplies the top two elements of the stack
+ * @stack: pointer to the stack
+ * @line_number: line number of the instruction
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	top = *stack;
+	top->next->n *= top->n;
+
+	*stack = top->next;
+	(*stack)->prev = NULL;
+
+	free(top);
+}
