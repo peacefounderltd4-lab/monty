8ed81cebdeb378e6d21540ef3540d39e93f071f8
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 18:50:24 +0200

    Add files via upload

---
+++ execute.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+ * execute_file - executes a Monty file
+ * @file: file containing instructions
+ *
+ * Return: nothing
+ */
+void execute_file(FILE *file)
+{
+	char *line;
+	char *opcode;
+	size_t buffer_size;
+	ssize_t read;
+	unsigned int line_number;
+	stack_t *stack;
+	void (*func)(stack_t **, unsigned int);
+
+	line = NULL;
+	buffer_size = 0;
+	line_number = 0;
+	stack = NULL;
+
+	while ((read = getline(&line, &buffer_size, file)) != -1)
+	{
+		line_number++;
+
+		if (read > 0 && line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		opcode = strtok(line, " \t");
+
+		if (opcode == NULL)
+			continue;
+
+		func = get_op_func(opcode);
+
+		if (func == NULL)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n",
+					line_number, opcode);
+			free(line);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+
+		func(&stack, line_number);
+	}
+
+	free(line);
+	free_stack(stack);
+}
