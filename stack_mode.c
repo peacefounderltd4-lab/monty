c5f1cdb1739a0b0aee3366b987ef0301a3e721f2
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 19:34:36 +0200

    Implement stack_mode function and flag
    Added a function to set the stack mode and a global variable to track the mode.

--- stack_mode.c
+++ stack_mode.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * stack_mode - sets the stack mode
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
 void stack_mode(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
@@ -7,3 +12,6 @@ void stack_mode(stack_t **stack, unsigned int line_number)
 
 	stack_mode_flag = 0;
 }
+
+/* Global variable used to select stack or queue mode */
+int stack_mode_flag = 0;
