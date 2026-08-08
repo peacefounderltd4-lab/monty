99fc27e63f5d6f05d22b1c9f8cb878331739020e
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 19:35:18 +0200

    Document queue_mode function
    Add documentation for queue_mode function.

--- queue_mode.c
+++ queue_mode.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * queue_mode - sets the queue mode
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
 void queue_mode(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
