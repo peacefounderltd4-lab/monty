71026c8084283346532ffe81fc0149f5f63fc762
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 19:21:23 +0200

    Implement swap function for stack

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
