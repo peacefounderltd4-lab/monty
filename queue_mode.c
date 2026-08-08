c3cf505990b339b03822ddaf15dc5d62ce39212d
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 19:54:02 +0200

    Refactor queue_mode function documentation
    Updated function documentation for clarity.

--- queue_mode.c
+++ queue_mode.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 
 /**
- * queue_mode - sets the queue mode
- * @stack: pointer to the stack
+ * queue_mode - sets queue mode
+ * @stack: pointer to stack
  * @line_number: line number
  */
 void queue_mode(stack_t **stack, unsigned int line_number)
