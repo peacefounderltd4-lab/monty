c429e90c0465c56e883cc51f09e56ae5298fef83
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 18:51:36 +0200

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * get_op_func - selects the function for an opcode
+ * @opcode: opcode
+ *
+ * Return: pointer to the appropriate function, or NULL
+ */
+void (*get_op_func(char *opcode))(stack_t **, unsigned int)
+{
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", div_op},
+		{"mul", mul},
+		{"mod", mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+			return (instructions[i].f);
+	}
+
+	return (NULL);
+}
