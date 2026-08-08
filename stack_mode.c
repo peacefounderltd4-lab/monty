b11e15af85851360613ce1e523b19a37849a4893
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 19:52:52 +0200

    Refactor stack_mode function and comments
    Removed unnecessary comments and adjusted formatting.

--- stack_mode.c
+++ stack_mode.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 
 /**
- * stack_mode - sets the stack mode
- * @stack: pointer to the stack
+ * stack_mode - sets stack mode
+ * @stack: pointer to stack
  * @line_number: line number
  */
 void stack_mode(stack_t **stack, unsigned int line_number)
@@ -12,6 +12,3 @@ void stack_mode(stack_t **stack, unsigned int line_number)
 
 	stack_mode_flag = 0;
 }
-
-/* Global variable used to select stack or queue mode */
-int stack_mode_flag = 0;
