36fa618be04abb9833866c4a3cf3d1dcace1f77b
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-09 09:42:50 +0200

    Refactor queue_mode function
    Removed unnecessary comments and whitespace in queue_mode function.

--- queue_mode.c
+++ queue_mode.c
@@ -1,14 +1,8 @@
 #include "monty.h"
 
-/**
- * queue_mode - sets queue mode
- * @stack: pointer to stack
- * @line_number: line number
- */
 void queue_mode(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
-
 	stack_mode_flag = 1;
 }
