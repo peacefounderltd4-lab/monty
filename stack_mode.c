223b918e84fa609bee356b89e9b4fe401e2f3d13
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-09 09:42:05 +0200

    Remove stack_mode function documentation and clean up

--- stack_mode.c
+++ stack_mode.c
@@ -1,14 +1,10 @@
 #include "monty.h"
 
-/**
- * stack_mode - sets stack mode
- * @stack: pointer to stack
- * @line_number: line number
- */
+int stack_mode_flag = 0;
+
 void stack_mode(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
-
 	stack_mode_flag = 0;
 }
