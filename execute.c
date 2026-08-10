ab1bdcd1777d86c3051b781bafaa8958f049e3f7
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-10 08:33:37 +0200

    Refactor execute function and remove execute_brainfuck
    Refactor execute function to ignore empty lines and comments. Remove execute_brainfuck function and its associated logic.

--- execute.c
+++ execute.c
@@ -28,7 +28,16 @@ void execute_file(FILE *file)
 		if (read > 0 && line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		opcode = strtok(line, " \t");
+		/* Ignore empty lines and comments */
+		opcode = line;
+
+		while (*opcode == ' ' || *opcode == '\t')
+			opcode++;
+
+		if (*opcode == '\0' || *opcode == '#')
+			continue;
+
+		opcode = strtok(opcode, " \t");
 
 		if (opcode == NULL)
 			continue;
@@ -49,128 +58,4 @@ void execute_file(FILE *file)
 
 	free(line);
 	free_stack(stack);
-}
-
-/**
- * execute_brainfuck - executes a Brainf*ck program
- * @file: file containing Brainf*ck instructions
- *
- * Return: nothing
- */
-void execute_brainfuck(FILE *file)
-{
-	char *program;
-	size_t size;
-	size_t len;
-	size_t i;
-	size_t ptr;
-	unsigned char *memory;
-	int loop;
-
-	program = NULL;
-	size = 0;
-	len = 0;
-	ptr = 0;
-
-	while (getline(&program, &size, file) != -1)
-	{
-		len += strlen(program);
-	}
-
-	free(program);
-	rewind(file);
-
-	program = malloc(len + 1);
-	if (program == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	if (fread(program, 1, len, file) != len)
-	{
-		free(program);
-		fprintf(stderr, "Error: can't read file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	program[len] = '\0';
-
-	memory = calloc(30000, sizeof(unsigned char));
-	if (memory == NULL)
-	{
-		free(program);
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	i = 0;
-
-	while (i < len)
-	{
-		switch (program[i])
-		{
-		case '>':
-			ptr++;
-			break;
-
-		case '<':
-			if (ptr > 0)
-				ptr--;
-			break;
-
-		case '+':
-			memory[ptr]++;
-			break;
-
-		case '-':
-			memory[ptr]--;
-			break;
-
-		case '.':
-			putchar(memory[ptr]);
-			break;
-
-		case ',':
-			memory[ptr] = getchar();
-			break;
-
-		case '[':
-			if (memory[ptr] == 0)
-			{
-				loop = 1;
-				while (loop && ++i < len)
-				{
-					if (program[i] == '[')
-						loop++;
-					else if (program[i] == ']')
-						loop--;
-				}
-			}
-			break;
-
-		case ']':
-			if (memory[ptr] != 0)
-			{
-				loop = 1;
-				while (loop && i > 0)
-				{
-					i--;
-					if (program[i] == ']')
-						loop++;
-					else if (program[i] == '[')
-						loop--;
-				}
-			}
-			break;
-
-		default:
-			break;
-		}
-
-		i++;
-	}
-
-	free(memory);
-	free(program);
 }
